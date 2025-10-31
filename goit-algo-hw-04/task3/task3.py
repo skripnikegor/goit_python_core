86de712f1f0fb06f531774d282ca0fa039b09176
Egor <34675756+skripnikegor@users.noreply.github.com>
2025-10-31 18:53:25 +0200

    HW4. Task3
    Rewrited task 3. Finished

--- goit-algo-hw-04/task3/task3.py
+++ goit-algo-hw-04/task3/task3.py
@@ -46,50 +46,55 @@ python hw03.py /шлях/до/вашої/директорії
 скрипт повинен вивести схожу структуру:
 
 """
-import sys, colorama
+import sys
 from pathlib import Path
+from colorama import Fore
 
-def get_files_from_path(filename: str) -> list:
+def print_directory_structure(filepath: Path, separator: str=""):
     """
-    
-    
+    Print files from directory by path. If filepath is dir - run itself recursive.
+
+    Args:
+        path - path to the file.
+        separator - symbol before file name
+
     """
-    directory = Path(filename)
-    if not directory.exists() or not directory.is_dir():
-        pass
-    result = []
-    for way in directory.iterdir():
-        result.append(str(way.name))
-        if way.is_dir():
-            result.append(get_files_from_path(way))
-    return result
-
-def render(files, indent=0):
-    text = ""
-    for f in files:
-        if isinstance(f, str):
-            text += "    " * indent + f + "\n"
-        elif isinstance(f, list):
-            # рекурсивный вызов для вложенных списков
-            text += render(f, indent + 1)
-    return text
+    for item in filepath.iterdir():
+        if item.is_dir():
+            print(f"{separator}{Fore.BLUE}{item.name}/")
+            print_directory_structure(item, separator + "   ")
+        else:
+            print(f"{separator}{Fore.GREEN}{item.name}")
 
 
-def get_files_from_directory(filename: str) -> str:
+def get_files_from_directory():
     """"
-    
-    
-    
+    Checks if directory exist and it is directory. Prints file structure by directory. 
+
+    Args:
+        path - get path to folder as script start parameter.
+
+    Raises:
+        if path/data wrong returns an empty array.
     """
+    if len(sys.argv) < 2:
+        print(f"{Fore.RED} Error. Please add path to the directory.")
+        print(f"{Fore.YELLOW} Example: python task3.py C:/Users/Admin/Pictures")
+        sys.exit(1)
 
-    files = get_files_from_path(filename)
-    return render(files)
+    directory = Path(sys.argv[1])
 
+    if not directory.exists():
+        print(f"{Fore.RED}Error '{directory}' not exist.")
+        sys.exit(1)
 
+    if not directory.is_dir():
+        print(f"{Fore.RED}Error: '{directory}' is not directory.")
+        sys.exit(1)
 
+    # Print root directory
+    print(f"{Fore.CYAN}{directory.name}/")
+    print_directory_structure(directory, separator="    ")
 
-# if __name__ == "__taks3__":
-#     pass
-
-files = get_files_from_directory("C:/Users/yskrypnyk/Desktop")
-print(files)
+if __name__ == "__main__":
+    get_files_from_directory()
